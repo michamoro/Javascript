7295d4b044aa9b5f1666d0808c81735df9433de4
Moro <mimoro1176@hotmail.com>
2020-07-30 11:59:39 -0500

    tipos, literales y bibliografia

---
+++ 02DataTypes/literales.js
@@ -0,0 +1,12 @@
+/* representar valores en JavaScript. */
+var literalArray = ["Mar", "José", "Lucía"];
+var literalBuleano = true;
+var literalEntero = 036;
+var literalPuntoFlotante = 3.1415;
+var literalObjeto = {
+    ruedas: 4,
+    color: "rojo",
+    "max velocidad": 120,
+    combustible: "Diesel"
+}
+var literalString = "José Sáez";
---
+++ 02DataTypes/types.js
@@ -0,0 +1,23 @@
+buleano = true;
+buleano = false; // valor por defecto
+nulo = null;
+var andifaind;
+var namber = 1234567;
+var bigInt = 9007199254740992n;
+var estring = 'sample'
+
+var simnbol = Symbol('foo') // únicos e immutables
+simnbol = 'text'
+console.log(simnbol);
+
+class obyect {
+    a = 1;
+    b = '2';
+    c = true;
+}
+var obllect = new obyect();
+console.log(obllect.a);
+
+var arrai = ['a', 'b', 2]
+var dicshionari = {a: 1, b: 2, c: 'c'} // clave - valor
+
